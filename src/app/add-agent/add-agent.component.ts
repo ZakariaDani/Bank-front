e5cc4badc3800c266af5fd69ff38a24d04bf9b3e
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-05-04 14:48:30 +0000

    add-dialog-to-back-office-create-agent

---
+++ src/app/add-agent/add-agent.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-agent',
+  templateUrl: './add-agent.component.html',
+  styleUrls: ['./add-agent.component.css']
+})
+export class AddAgentComponent implements OnInit {
+  firstName:any;
+  LastName: any;
+  dateOfBirth:any;
+  adress:any;
+  email:any;
+  phone:any;
+  matricule:any;
+  patente:any;
+  description:any;
+  file:any;
+  constructor(public dialogAdd: MatDialogRef<AddAgentComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  OnCancel() {
+    this.dialogAdd.close();
+  }
+  create() {
+    this.dialogAdd.close();
+  }
+}
